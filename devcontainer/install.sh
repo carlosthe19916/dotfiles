5bda69418aae45ec2c1c7c34c9f51d0ee92a65e6
Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>
2025-11-04 09:17:17 -0500

    Avoid errors for directories

--- devcontainer/install.sh
+++ devcontainer/install.sh
@@ -6,5 +6,8 @@ echo "cd /workspace" >>~/.bashrc
 # Enable git autocompletion
 echo "source /usr/share/bash-completion/completions/git" >> ~/.bashrc
 
+mkdir -p $HOME/.npm
+mkdir -p $HOME/.local
+
 sudo chown -R $USER:$USER $HOME/.npm
 sudo chown -R $USER:$USER $HOME/.local
