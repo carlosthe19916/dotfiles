e9c981cc5d2231946af3af544f97d2232bf5916a
Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>
2025-11-04 08:25:19 -0500

    Enable auto completion to git

--- devcontainer/install.sh
+++ devcontainer/install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# Locate bash at /workspace
 echo "cd /workspace" >>~/.bashrc
+
+# Enable git autocompletion
+echo "source /usr/share/bash-completion/completions/git" >> ~/.bashrc
 
 sudo chown -R $USER:$USER $HOME/.npm
 sudo chown -R $USER:$USER $HOME/.local
